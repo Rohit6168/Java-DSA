12ea4581e88a6e81ced568eeaa8063e7f27a2c1d
Rohit6168 <rohitlokhande616@gmail.com>
2026-01-05 22:54:43 +0530

    Added Binary Search

---
+++ Array/BinarySearch.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class BinarySearch {
+
+    public static int binarySearch(int numbers[],int key){
+        int start =0;
+        int end =numbers.length-1;
+
+        while (start<=end) {
+            int mid =(start+end)/2;
+            if(numbers[mid]==key){
+                return mid;
+            }
+            if(numbers[mid]<key){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10,12,14,16};
+        int key =14;
+
+        int index =binarySearch(numbers, key);
+        if(index == -1){
+            System.out.println("key not found");
+        }else{
+            System.out.println("key found at index: " +index);
+        }
+    }
+}
