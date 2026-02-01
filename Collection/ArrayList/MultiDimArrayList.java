7a8ca22ea3d3646dafe684c84b08f63279c6a50e
Rohit6168 <rohitlokhande616@gmail.com>
2026-02-01 12:51:02 +0530

    Day-13

---
+++ Collection/ArrayList/MultiDimArrayList.java
@@ -0,0 +1,54 @@
+package Collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class MultiDimArrayList {
+    public static void main(String[] args) {
+        // ArrayList<ArrayList<Integer>>mainList=new ArrayList<>();
+        // ArrayList<Integer>list1=new ArrayList<>();
+        // list1.add(1);
+        // list1.add(2);
+        // mainList.add(list1);
+
+        // ArrayList<Integer>list2=new ArrayList<>();
+        // list2.add(3);
+        // list2.add(4);
+        // mainList.add(list2);
+
+        // System.out.println(mainList);
+
+        // for(int i=0;i<mainList.size();i++){
+        //     ArrayList<Integer>currList=mainList.get(i);
+        //     for(int j=0;j<currList.size();j++){
+        //         System.out.print(currList.get(j)+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        ArrayList<ArrayList<Integer>>mainList=new ArrayList<>();
+        ArrayList<Integer>list1=new ArrayList<>();
+        ArrayList<Integer>list2=new ArrayList<>();
+        ArrayList<Integer>list3=new ArrayList<>();
+
+        for(int i=1;i<=5;i++){
+            list1.add(i*1);
+            list2.add(i*2);
+            list3.add(i*3);
+        }
+        mainList.add(list1);
+        mainList.add(list2);
+        mainList.add(list3);
+        System.out.println(mainList);
+
+        for(int i=0;i<mainList.size();i++){
+            ArrayList<Integer>currList=mainList.get(i);
+            for(int j=0;j<currList.size();j++){
+                System.out.print(currList.get(j)+" ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
---
+++ Collection/ArrayList/PairSum1.java
@@ -0,0 +1,52 @@
+package Collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class PairSum1 {
+
+    // O(n^2)
+    // check pair of sum exist in list or not
+    // public static boolean pairexist( ArrayList<Integer>list,int target){
+
+    //     for(int i=0;i<list.size();i++){
+    //         for(int j=0;j<list.size();j++){
+    //             if(list.get(i)+list.get(j)==target){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+
+    //Two pointer approach---->O(n)
+    public static boolean pairexist( ArrayList<Integer>list,int target){
+        int lp=0;
+        int rp=list.size()-1;
+        while (lp<rp) {
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }
+
+            if(list.get(lp)+list.get(rp)<target){
+                lp++;
+            }else{
+                rp--;
+            }
+            
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        int target=5;
+        System.out.println(pairexist(list, target));
+
+    }
+}
